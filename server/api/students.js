056ab8368fbbcad80b66bff3900321758ffa2f20
Max Yedid <maxyedid1@gmail.com>
2022-01-21 14:49:46 -0500

    Added the students API directory table

---
+++ server/api/students.js
@@ -0,0 +1,23 @@
+const router = require('express').Router()
+
+const {Student} = require('../db')
+
+router.get('/', async (req, res) => {
+    try {
+        const students = await Student.findAll()
+        res.status(200).send(students)
+    } catch (error) {
+        res.status(404).send(error.message)
+    }
+})
+
+router.post('/', async (req, res) => {
+    try {
+        const newStudent = await Student.create(req.body)
+        res.json(newStudent)
+    } catch (error) {
+        res.send(error.message)
+    }
+})
+
+module.exports = router;
