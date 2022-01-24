fad5c92e12863611b51d596e062a6fbb03273ec5
Max Yedid <maxyedid1@gmail.com>
2022-01-23 23:58:38 -0500

    Converted parameters in database.js to be a postgres url instead

--- server/db/database.js
+++ server/db/database.js
@@ -4,7 +4,7 @@ const pkg = require('../../package.json')
 
 
 //Creating database url for the api
-const db = new Sequelize('students', 'postgres', 'ready482', {
+const db = new Sequelize('postgres://postgres:ready482@localhost:5432/students', {
         host: 'localhost',
         dialect: 'postgres'
         }
